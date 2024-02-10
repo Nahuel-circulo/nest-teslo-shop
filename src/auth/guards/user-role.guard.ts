77119e18c8922fe50a6b7b656abf4cdcfa688f8c
Nahuel-circulo <pedroso.nahuel.dev@gmail.com>
2024-02-10 12:00:21 -0300

    create custom guard

--- src/auth/guards/user-role.guard.ts
+++ src/auth/guards/user-role.guard.ts
@@ -1,22 +1,37 @@
-import {Reflector} from '@nestjs/core';
+import { Reflector } from '@nestjs/core';
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { Observable } from 'rxjs';
+import { BadRequestException, ForbiddenException } from '@nestjs/common/exceptions';
 
 @Injectable()
 export class UserRoleGuard implements CanActivate {
   constructor(
+    // para obtener la metadata
     private readonly reflector: Reflector,
     //private readonly allowedRoles: Array<string>,
-  ){}
+  ) { }
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
 
+    // los roles se encuentran en el decorador de setMetadata
+    const validRoles: string[] = this.reflector.get('roles', context.getHandler())
 
-    const validRoles: string[] = this.reflector.get('roles',context.getHandler())
+    if (!validRoles) return true;
+    if (validRoles.length === 0) return true;
 
-    console.log(validRoles);
 
-    return true;
+    const req = context.switchToHttp().getRequest()
+    const user = req.user;
+
+    if(!user) throw new BadRequestException('User not found');
+    for (const role of user.roles) {
+      if(validRoles.includes(role)){
+        return true;
+      }
+    }
+
+
+    throw new ForbiddenException(`User ${user.fullName} need a valid role: [${validRoles}]`);
   }
 }
