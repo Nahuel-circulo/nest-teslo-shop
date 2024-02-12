65a6739043ac6c946ec6fdcb586716634aae5240
Nahuel-circulo <pedroso.nahuel.dev@gmail.com>
2024-02-11 23:39:22 -0300

    document entity properties

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,48 +1,106 @@
 import { BeforeInsert, BeforeUpdate, Column, Entity, ManyToOne, OneToMany, PrimaryGeneratedColumn } from "typeorm";
 import { ProductImage } from "./product-image.entity";
 import { User } from "src/auth/entities/user.entity";
+import { ApiProperty } from "@nestjs/swagger";
 
-@Entity({name:'products'})
+@Entity({ name: 'products' })
 export class Product {
 
+    @ApiProperty({
+        example: 'd9e4a3b2-a3b2-c3d4-e5f6-a3b2c3d4e5f6',
+        description: 'Product ID',
+        uniqueItems: true
+    })
     @PrimaryGeneratedColumn('uuid')
     id: string;
 
+    @ApiProperty({
+        example: 'T-shirt Teslo',
+        description: 'Product Title',
+        uniqueItems: true
+    })
     @Column('text', {
         unique: true
     })
     title: string;
 
+    @ApiProperty(
+        {
+            example: 0,
+            description: 'Product Price',
+        }
+    )
     @Column('float', {
         default: 0
     })
     price: number
 
+    @ApiProperty({
+        example: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.',
+        description: 'Product Description',
+        default:null
+    })
     @Column({
         type: 'text',
         nullable: true
     })
     description: string;
 
+    @ApiProperty(
+        {
+            example: 't_shirt_teslo',
+            description: 'Product SLUG',
+            uniqueItems:true
+        }
+    )
     @Column({
         type: 'text',
         unique: true
     })
     slug: string;
 
+    @ApiProperty({
+        example: 1,
+        description: 'Product Stock',
+        default:0
+    })
     @Column('int', {
         default: 0
     })
     stock: number;
 
+    @ApiProperty({
+        example: [
+            "XS",
+            "S",
+            "M",
+            "L",
+            "XL",
+            "XXL"
+          ],
+        description: 'Product sizes',
+    })
     @Column('text', {
         array: true
     })
     sizes: string[];
 
+    @ApiProperty(
+        {
+            example: 'unisex',
+            description: 'Product Gender',
+        }
+    )
     @Column('text')
     gender: string;
 
+    @ApiProperty(
+        {
+            example: ['hoddie'],
+            description: 'Product Tags',
+            default:[]
+        }
+    )
     @Column('text', {
         array: true,
         default: []
@@ -51,7 +109,7 @@ export class Product {
 
     @ManyToOne(
         () => User,
-        (user)=>user.product,
+        (user) => user.product,
         { eager: true } //eager carga las relaciones
     )
     user: User;
@@ -59,10 +117,18 @@ export class Product {
     //eager carga las relaciones
     //eager no funciona en querybuilder
     //uno a muchos
+    @ApiProperty({
+        example:[
+            "8529312-00-A_0_2000.jpg",
+            "8529312-00-A_1.jpg"
+          ],
+          default:[],
+          description:'Product Images'
+    })
     @OneToMany(
         () => ProductImage,
         (productImage) => productImage.product,
-        { cascade: true,eager:true }
+        { cascade: true, eager: true }
     )
     images?: ProductImage[];
 
