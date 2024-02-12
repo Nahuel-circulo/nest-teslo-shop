8bdbd9c826f73fe54b0571d79152a4c8e1288149
Nahuel-circulo <pedroso.nahuel.dev@gmail.com>
2024-02-11 23:46:29 -0300

    document Dtos

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,6 +1,7 @@
 
 // como recibe la data desde las consultas
 
+import { ApiProperty } from "@nestjs/swagger";
 import {
     IsArray,
     IsIn,
@@ -15,42 +16,55 @@ import {
 export class CreateProductDto {
 
 
+    @ApiProperty({
+        description: 'Product title (unique)',
+        nullable: false,
+        minLength: 1,
+    })
     @IsString()
     @MinLength(1)
     title: string;
 
+    @ApiProperty()
     @IsPositive()
     @IsOptional()
     @IsNumber()
     price?: number;
 
+    @ApiProperty()
     @IsString()
     @IsOptional()
     description?: string;
 
+    @ApiProperty()
     @IsString()
     @IsOptional()
     slug?: string;
 
+    @ApiProperty()
     @IsInt()
     @IsPositive()
     @IsOptional()
     stock?: number;
 
+    @ApiProperty()
     @IsArray()
     @IsString({ each: true })
     sizes: string[];
 
    
+    @ApiProperty()
     @IsIn(['men','women','kids','unisex'])
     gender: string;
 
+    @ApiProperty()
     @IsArray()
     @IsString({ each: true })
     @IsOptional()
     tags: string[];
 
 
+    @ApiProperty()
     @IsArray()
     @IsString({ each: true })
     @IsOptional()
