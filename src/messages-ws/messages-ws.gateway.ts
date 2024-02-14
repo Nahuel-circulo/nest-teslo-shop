9c4c41b34069e37b7418c3f04334d910d534e956
Nahuel-circulo <pedroso.nahuel.dev@gmail.com>
2024-02-13 23:17:58 -0300

    connect when jwt is valid

--- src/messages-ws/messages-ws.gateway.ts
+++ src/messages-ws/messages-ws.gateway.ts
@@ -2,17 +2,31 @@ import { OnGatewayConnection, OnGatewayDisconnect, SubscribeMessage, WebSocketGa
 import { MessagesWsService } from './messages-ws.service';
 import { Server, Socket } from 'socket.io';
 import { NewMessageDto } from './dtos/new-message.dto';
+import { JwtService } from '@nestjs/jwt';
+import { JwtPayload } from 'src/auth/interfaces';
 
 @WebSocketGateway({ cors: true })
 export class MessagesWsGateway implements OnGatewayConnection, OnGatewayDisconnect {
 
   @WebSocketServer() wss: Server;
 
-  constructor(private readonly messagesWsService: MessagesWsService) { }
+  constructor(
+    private readonly messagesWsService: MessagesWsService,
+    private readonly jwtService: JwtService) { }
 
   handleConnection(client: Socket) {
     const token = client.handshake.headers.authentication as string;
-    console.log(token)
+    let payload: JwtPayload;
+
+    try {
+      payload = this.jwtService.verify(token);
+    } catch (error) {
+      client.disconnect();
+      console.log('hola')
+      return 
+
+    }
+    console.log({payload})
     this.messagesWsService.registerClient(client)
     this.wss.emit('clients-updated', this.messagesWsService.getConnectedClients())
 
@@ -27,15 +41,15 @@ export class MessagesWsGateway implements OnGatewayConnection, OnGatewayDisconne
 
     // Emite unicamente al cliente
     // client.emit('message-from-server', {fullName:'soy yo',payload: payload.message || 'no-message!!'})
-    
+
     // Emite a todos menos al cliente
     // client.broadcast.emit('message-from-server', {fullName:'soy yo',payload: payload.message || 'no-message!!'})
-  
+
     // Emite a todos los clientes
     this.wss.emit('message-from-server', {
       fullName: 'soy yo',
       message: payload.message || 'no-message!!'
     })
   }
-  
+
 }
